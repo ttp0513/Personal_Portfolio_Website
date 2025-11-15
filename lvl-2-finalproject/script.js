1b6cb19f37160a9b83434d420490554c0f81d58e
ttp0513 <johnphanthe@gmail.com>
2025-11-15 00:36:47 -0500

    Update the Menu items for Special dishes

--- lvl-2-finalproject/script.js
+++ lvl-2-finalproject/script.js
@@ -23,3 +23,54 @@ window.addEventListener('scroll', () => {
 });
 
 
+// Fetch Menu Data from menu.json and log it to the console
+const menuContainer = document.querySelector('.menu-container');
+const special = document.querySelector('.menu-button.special');
+special.addEventListener('click', () => {
+    menuContainer.innerHTML = '';
+    async function loadMenu() {
+    try {
+    const res = await fetch('./menu.json');
+    const data = await res.json();
+    const specials = data.specials;
+    specials.forEach(({ name, description, price, image_url }) => {
+        menuContainer.innerHTML += `
+
+        <div class="menu_card">
+
+                <div class="menu_image">
+                    <img src="${image_url}" alt="${name}" title="${name}"/>
+                </div>
+
+                <div class="small_card">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+
+                <div class="menu_info">
+                    <h2>${name}</h2>
+                    <p>
+                        ${description}
+                    </p>
+                    <h3>${price.toFixed(2)}</h3>
+                    <div class="menu_icon">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                    </div>
+                    <a href="#" class="menu_btn">Order</a>
+                </div>
+
+            </div> 
+        `;
+    });
+  } catch (err) {
+    console.error('Error loading menu:', err);
+  }
+}
+loadMenu();
+}
+);
+
+
