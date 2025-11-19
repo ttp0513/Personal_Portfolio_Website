5ce328a3968e10399d74c1525aa0eb75b1df1615
ttp0513 <johnphanthe@gmail.com>
2025-11-18 23:09:04 -0500

    Styled Menu Container and add menu items using my own APIs in menu.json file

--- lvl-2-finalproject/script.js
+++ lvl-2-finalproject/script.js
@@ -22,13 +22,11 @@ window.addEventListener('scroll', () => {
     };
 });
 
-
-// Fetch Menu Data from menu.json and log it to the console
+// Default Menu Items Load - Specials
 const menuContainer = document.querySelector('.menu-container');
-const special = document.querySelector('.menu-button.special');
-special.addEventListener('click', () => {
     menuContainer.innerHTML = '';
-    async function loadMenu() {
+    // Fetch Menu Data from menu.json and log it to the console
+    async function loadSpecialMenu() {
     try {
     const res = await fetch('./menu.json');
     const data = await res.json();
@@ -37,13 +35,8 @@ special.addEventListener('click', () => {
         menuContainer.innerHTML += `
 
         <div class="menu_card">
-
                 <div class="menu_image">
                     <img src="${image_url}" alt="${name}" title="${name}"/>
-                </div>
-
-                <div class="small_card">
-                    <i class="fa-solid fa-heart"></i>
                 </div>
 
                 <div class="menu_info">
@@ -51,15 +44,8 @@ special.addEventListener('click', () => {
                     <p>
                         ${description}
                     </p>
-                    <h3>${price.toFixed(2)}</h3>
-                    <div class="menu_icon">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star-half-stroke"></i>
-                    </div>
-                    <a href="#" class="menu_btn">Order</a>
+                    <h3>$${price.toFixed(2)}</h3>
+                    <a href="" class="menu_btn">Order</a>
                 </div>
 
             </div> 
@@ -69,8 +55,126 @@ special.addEventListener('click', () => {
     console.error('Error loading menu:', err);
   }
 }
-loadMenu();
+loadSpecialMenu();
+                
+// Event Listener for Specials Menu Button
+const special = document.querySelector('.menu-button.special');
+special.addEventListener('click', () => {
+    menuContainer.innerHTML = '';
+    loadSpecialMenu();
 }
 );
 
+// Event Listener for Snacks Menu Button
+const snacks = document.querySelector('.menu-button.snack');
+snacks.addEventListener('click', () => {
+    const scrollY = window.scrollY;
+    menuContainer.innerHTML = '';
+    // Fetch Menu Data from menu.json and log it to the console
+    async function loadSnackMenu() {
+    try {
+    const res = await fetch('./menu.json');
+    const data = await res.json();
+    const snacks = data.snacks;
+    snacks.forEach(({ name, description, price, image_url }) => {   
+        menuContainer.innerHTML += `
+        <div class="menu_card">
+                <div class="menu_image">
+                    <img src="${image_url}" alt="${name}" title="${name}"/>
+                </div>
+                <div class="menu_info">
+                    <h2>${name}</h2>
+                    <p>
+                        ${description}
+                    </p>
+                    <h3>$${price.toFixed(2)}</h3>
+                    <a href="" class="menu_btn">Order</a>
+                </div>
 
+            </div> 
+        `;
+    });
+  } catch (err) {
+    console.error('Error loading menu:', err);
+  }
+}
+loadSnackMenu();
+window.scrollTo(0, scrollY);
+}
+);
+
+// Event Listener for Desserts Menu Button
+const dessert = document.querySelector('.menu-button.dessert');
+dessert.addEventListener('click', () => {
+    const scrollY = window.scrollY;
+    menuContainer.innerHTML = '';
+    // Fetch Menu Data from menu.json and log it to the console
+    async function loadDessertMenu() {
+    try {
+    const res = await fetch('./menu.json');
+    const data = await res.json();
+    const desserts = data.dessert;
+    desserts.forEach(({ name, description, price, image_url }) => {   
+        menuContainer.innerHTML += `
+        <div class="menu_card">
+                <div class="menu_image">
+                    <img src="${image_url}" alt="${name}" title="${name}"/>
+                </div>
+                <div class="menu_info">
+                    <h2>${name}</h2>
+                    <p>
+                        ${description}
+                    </p>
+                    <h3>$${price.toFixed(2)}</h3>
+                    <a href="" class="menu_btn">Order</a>
+                </div>
+
+            </div> 
+        `;
+    });
+  } catch (err) {
+    console.error('Error loading menu:', err);
+  }
+}
+loadDessertMenu();
+window.scrollTo(0, scrollY);
+}
+);
+
+// Event Listener for Drinks Menu Button
+const drink = document.querySelector('.menu-button.drink');
+drink.addEventListener('click', () => {
+    const scrollY = window.scrollY;
+    menuContainer.innerHTML = '';
+    // Fetch Menu Data from menu.json and log it to the console
+    async function loadDrinkMenu() {
+    try {
+    const res = await fetch('./menu.json');
+    const data = await res.json();
+    const drinks = data.drink;
+    drinks.forEach(({ name, description, price, image_url }) => {   
+        menuContainer.innerHTML += `
+        <div class="menu_card">
+                <div class="menu_image">
+                    <img src="${image_url}" alt="${name}" title="${name}"/>
+                </div>
+                <div class="menu_info">
+                    <h2>${name}</h2>
+                    <p>
+                        ${description}
+                    </p>
+                    <h3>$${price.toFixed(2)}</h3>
+                    <a href="" class="menu_btn">Order</a>
+                </div>
+                
+            </div> 
+        `;
+    });
+    } catch (err) {
+    console.error('Error loading menu:', err);
+    }
+    }
+    loadDrinkMenu();
+    window.scrollTo(0, scrollY);
+}
+);
